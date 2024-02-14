f877b60ee02122ed2b490541bac66dafc3c71957
Justin Griffin <jgriffin555@comcast.net>
2024-02-14 13:55:43 -0600

    feat: add ledger module to network module (needs refactor)

--- server/src/services/network.service.ts
+++ server/src/services/network.service.ts
@@ -2,11 +2,13 @@ import { Base, Network as ManyNetwork } from "@liftedinit/many-js";
 import { Injectable } from "@nestjs/common";
 import { Blockchain } from "../utils/network/blockchain";
 import { Events } from "../utils/network/events";
+import { Ledger } from "../utils/network/ledger";
 
 type N = ManyNetwork & {
   base: Base;
   blockchain: Blockchain;
   events: Events;
+  ledger: Ledger;
 };
 
 @Injectable()
@@ -20,7 +22,7 @@ export class NetworkService {
     }
 
     const network = new ManyNetwork(url);
-    network.apply([Base, Blockchain, Events]);
+    network.apply([Base, Blockchain, Events, Ledger]);
     this.cache.set(url, network as N);
     return network as N;
   }
