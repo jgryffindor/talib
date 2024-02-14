59c076a319ec8c93d437602c0fb31e1d28ef4383
Justin Griffin <jgriffin555@comcast.net>
2024-02-13 23:36:07 -0600

    feat: import tokens module, service and entity to neighborhood module.

--- server/src/neighborhoods/neighborhood.module.ts
+++ server/src/neighborhoods/neighborhood.module.ts
@@ -1,5 +1,6 @@
 import { Module } from "@nestjs/common";
 import { TypeOrmModule } from "@nestjs/typeorm";
+import { Token } from "../database/entities/token.entity";
 import { Block } from "../database/entities/block.entity";
 import { Neighborhood } from "../database/entities/neighborhood.entity";
 import { TransactionDetails } from "../database/entities/transaction-details.entity";
@@ -12,6 +13,8 @@ import { BlockService } from "./blocks/block.service";
 import { EventsModule } from "./events/events.module";
 import { NeighborhoodController } from "./neighborhood.controller";
 import { NeighborhoodService } from "./neighborhood.service";
+import { TokenModule } from "./tokens/tokens.module";
+import { TokenService } from "./tokens/tokens.service";
 import { TransactionsModule } from "./transactions/transactions.module";
 
 @Module({
@@ -21,17 +24,20 @@ import { TransactionsModule } from "./transactions/transactions.module";
       Block,
       Transaction,
       TransactionDetails,
+      Token,
     ]),
     BlockModule,
     TransactionsModule,
     EventsModule,
     AddressesModule,
+    TokenModule,
   ],
   providers: [
     BlockService,
     NeighborhoodService,
     NetworkService,
     TxAnalyzerService,
+    TokenService
   ],
   controllers: [NeighborhoodController],
   exports: [NeighborhoodService, BlockService],
